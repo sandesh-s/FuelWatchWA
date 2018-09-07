a439691629845e09d510bdbe9cff9206c68391cf
Sandesh Satpute <sandesh.satpute@gmail.com>
2018-09-08 02:00:35 +0800

    test

--- FuelWatchWA.py
+++ FuelWatchWA.py
@@ -2,18 +2,11 @@ import feedparser
 from pprint import pprint
 f = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS')
 t = f.entries
-
-
-price = ["price1", "Price2"]
-price.append("price3")
-
 totalEntries = len(t)-1
 
 FuelEntries =[]
 i=0
 for i in range(totalEntries):
     FuelEntries.append(f.entries[i])
-
-
 #pprint(FuelEntries)
 pprint(FuelEntries)
