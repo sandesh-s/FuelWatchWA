2d65182fa9720258ea2909b252e2d0112ddcad79
sandycorebiker <sandesh.satpute@gmail.com>
2018-09-19 19:39:07 +0800

    sorting working correctly

--- FuelWatch_FunctionQuery.py
+++ FuelWatch_FunctionQuery.py
@@ -6,14 +6,14 @@ complete_feed = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuel
 
 fuelprice_list = []
 
-for entry in complete_feed['entries']:
-    fuel_dict = {}
-#fuelprice_list = [fuel_dict[entry["address"]]]
-
-    fuel_dict['address'] = entry["address"]
-    fuel_dict['price'] = entry["price"]
-
-    fuelprice_list.append(fuel_dict)
+# for entry in complete_feed['entries']:
+#     fuel_dict = {}
+# #fuelprice_list = [fuel_dict[entry["address"]]]
+#
+#     fuel_dict['address'] = entry["address"]
+#     fuel_dict['price'] = entry["price"]
+#
+#     fuelprice_list.append(fuel_dict)
 
 
 
@@ -26,9 +26,38 @@ def fuelbyproduct(productid):
 def fuelbyday(day):
     url = feedparser.parse('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Day='+day)
     return url
+
+def fuelDataBuilder(item):
+    item_list = []
+    for entry in item['entries']:
+        item_dict = {}
+        item_dict['address'] = entry["address"]
+        item_dict['price'] = entry["price"]
+        item_list.append(item_dict)
+    return item_list
+
+
+def fuelSorting(items):
+    return sorted(items, key = lambda sorteditems: sorteditems['price'])
+
+# def fuelSorting2(items):
+#     return sorted(items, key = lambda somefunction[])
+#
+# def some(items):
+#     return items['price']
 #-------------------------------------------------------------------------------
 
 todaysPrice = fuelbyday('today')
 tomorrowsPrice = fuelbyday('tomorrow')
+
+todaysPriceNotSorted = fuelDataBuilder(todaysPrice)
+tomorrowsPriceNotSorted = fuelDataBuilder(tomorrowsPrice)
+
+combinedPricesNotSorted = todaysPriceNotSorted + tomorrowsPriceNotSorted
+
+combinedPricesSorted = fuelSorting(combinedPricesNotSorted)
+
+pprint(combinedPricesSorted)
+
 
 #
